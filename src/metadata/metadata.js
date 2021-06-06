67da92ef29252485601df4c7a90e5bfd7ebc251a
Kawai Wong <zodiacdagreat@yahoo.com>
2021-06-07 11:26:36 +1200

    Restructure cv for position in same company

--- src/metadata/metadata.js
+++ src/metadata/metadata.js
@@ -67,84 +67,111 @@ module.exports = {
     }
   ],
   experience: [
-    {
-      title: 'Solutions Developer',
+    {      
       company: 'Fisher & Paykel Healthcare',
       location: 'Auckland',
-      start: 'January 2018',
-      end: 'Current',
-      blurb: 'Currently in the IoT team',
-      items: [
-        'Involved with and contributed towards the design of future system architecture and strategy',
-        'Develop and contribute towards system Platform libraries',
-        'Rapidly prototyped new concepts and ideas',
-        'Involved with the delivery of customer integration projects that allows our clients to access our patient management APIs',
-        'Developed a parser for the CoAP protocol for both UDP and TCP variants for NET',
-        'Co-developed a gateway for IoT devices that integrates with the in-house communications protocol',
-        'Co-developed various infrastructure Web APIs',
-        'Co-developed a dynamic serializer/deserializer library for Protobuf',        
-        'Involved with project delivery and coordination for the corporate website that required dealing with various stakeholders from Marketing Operations as well as a remote developer',
-        'Developed a custom module for the Kentico CMS used for bundling and packaging content for mobile apps',
-        'Create and build CI/CD pipelines for different projects',
-        'Set up automated testing infrastructure such as SpecFlow and various other tooling such as project templates and test harnesses',
-        'Developed new features for a world class Obstructive Sleep Apnea patient management and monitoring cloud based solution',
-        'Involved with the development and distribution of educational mobile apps for training purposes using React Native',
-        'Rewrote the existing user registration process for the SleepStyle iOS app to migrate from Objective-C to Swift and Storyboard',
-        'Involved with knowledge sharing of domain and system architecture to team members',
-        'Mentor junior members on best practices of software design and architecture',
-        'Experienced with design controls for medical devices'
-      ],
-      badges: ['C#', 'NET Core', 'ASP.NET Web API', 'ASP.NET Web Forms', 'SQL Server', 'CosmosDB', 'Dapper', 'AutoMapper', 'Windsor Castle', 'EntityFramework', 'Identity Server', 'SpecFlow', 'Infrastructure', 'Azure Event Hub', 'RabbitMQ', 'Redis', 'Azure DevOps CI/CD', 'Swift', 'Objective-C', 'React Native', 'Protobuf', 'CoAP', 'Design Controls', 'ISO27001']
+      roles: [
+        {
+          title: 'Senior Software Engineer',
+          start: 'May 2021',
+          end: 'Current',
+          blurb: 'Currently in the IoT team',
+          //blurb: 'Currently in the IoT team working on a next generation over-the-air solution for future FPH devices',
+          items: [
+            'Involved with and contributed towards the design of future system architecture and strategy',
+            'Mentor junior members on best practices of software design and architecture',
+            'Involved with hiring and recruitment',
+            'Involved with upgrading and dockerization of existing service components'
+          ]
+        },
+        {
+          title: 'Solutions Developer',
+          start: 'January 2018',
+          end: 'April 2021',
+          items: [
+            'Develop and contribute towards system Platform libraries',
+            'Rapidly prototyped new concepts and ideas',
+            'Involved with the delivery of customer integration projects that allows our clients to access our patient management APIs',
+            'Developed a parser for the CoAP protocol for both UDP and TCP variants for NET',
+            'Co-developed a gateway for IoT devices that integrates with the in-house communications protocol',
+            'Co-developed various infrastructure Web APIs',
+            //'Co-developed various infrastructure Web APIs for enabling a next generation over-the-air solution',
+            'Co-developed a dynamic serializer/deserializer library for Protobuf',        
+            'Involved with project delivery and coordination for the corporate website that required dealing with various stakeholders from Marketing Operations as well as a remote developer',
+            'Developed a custom module for the Kentico CMS used for bundling and packaging content for mobile apps',
+            'Create and build CI/CD pipelines for different projects',
+            'Set up automated testing infrastructure such as SpecFlow and various other tooling such as project templates and test harnesses',
+            'Developed new features for a world class Obstructive Sleep Apnea patient management and monitoring cloud based solution',
+            'Involved with the development and distribution of educational mobile apps for training purposes using React Native',
+            'Rewrote the existing user registration process for the SleepStyle iOS app to migrate from Objective-C to Swift and Storyboard',
+            'Involved with knowledge sharing of domain and system architecture to team members',
+            'Experienced with design controls for medical devices'
+          ],
+        }
+      ],      
+      badges: ['C#', 'NET Core', 'ASP.NET Web API', 'ASP.NET Web Forms', 'SQL Server', 'CosmosDB', 'Dapper', 'AutoMapper', 'Windsor Castle', 'EntityFramework', 'Identity Server', 'SpecFlow', 'Infrastructure', 'Docker', 'Azure Event Hub', 'RabbitMQ', 'Redis', 'Azure DevOps CI/CD', 'Swift', 'Objective-C', 'React Native', 'Protobuf', 'CoAP', 'Design Controls', 'ISO27001']
     },
-    {
-      title: 'Software Developer (ASB Bank)',
+    {      
       company: 'Datacom',
       location: 'Auckland',
-      start: 'February 2015',
-      end: 'December 2017',
-      blurb: 'Assigned as a Software Developer to work on ASB Bank\'s CRM system in the domain of home loan origination and customer onboarding',
-      items: [
-        'Designed and developed REST microservices for various CRM domains',
-        'Led the development and design of an automated test strategy for the CRM team',
-        'Designed and developed new components that added new functionality to the CRM',
-        'Involved as a lead developer and subject matter expert in large projects on multiple occassions to provide guidance and technical support',
-        'Delivered various compliance projects that are required by the Reserve Bank\'s legislative changes on time',
-        'Involved with a large-scale enterprise upgrade project for ASB\'s CRM system',
-        'Experienced working with enterprise HOST systems',
-        'Worked on business-as-usual enhancements and bugfixes for Onyx'
-      ],
+      roles: [
+        {
+          title: 'Software Developer (ASB Bank)',
+          start: 'February 2015',
+          end: 'December 2017',
+          blurb: 'Assigned as a Software Developer to work on ASB Bank\'s CRM system in the domain of home loan origination and customer onboarding',
+          items: [
+            'Designed and developed REST microservices for various CRM domains',
+            'Led the development and design of an automated test strategy for the CRM team',
+            'Designed and developed new components that added new functionality to the CRM',
+            'Involved as a lead developer and subject matter expert in large projects on multiple occassions to provide guidance and technical support',
+            'Delivered various compliance projects that are required by the Reserve Bank\'s legislative changes on time',
+            'Involved with a large-scale enterprise upgrade project for ASB\'s CRM system',
+            'Experienced working with enterprise HOST systems',
+            'Worked on business-as-usual enhancements and bugfixes for Onyx'
+          ]
+        }        
+      ],      
       badges: ['C#', 'NET Core', 'ASP.NET Web API', 'ASP.NET Web Forms', 'SQL Server', 'WCF', 'SpecFlow', 'Onyx CRM', 'Octopus Deploy', 'Integration'],
     },
-    {
-      title: 'Junior Developer',
+    {     
       company: 'ACIT',
       location: 'Auckland',
-      start: 'November 2012',
-      end: 'November 2014',
-      blurb: 'Developed and maintained customized business workflow management solutions and maternity management software for midwives.',
-      items: [
-        'Researched and developed an antenatal growth chart generator to integrate with the existing maternity management software',
-        'Built user interfaces for workflow management solutions and introduced a windows metro style design to new projects',
-        'Designed and developed reports for various workflow management systems',
-        'Develop and maintain customized workflow management software for various small to medium businesses',
-        'Performed data migration exercises when integrating or migrating from legacy systems',
-        'Worked with clients to troubleshoot and diagnose issues'
+      roles: [
+        {
+          title: 'Junior Developer',
+          start: 'November 2012',
+          end: 'November 2014',
+          blurb: 'Developed and maintained customized business workflow management solutions and maternity management software for midwives.',
+          items: [
+            'Researched and developed an antenatal growth chart generator to integrate with the existing maternity management software',
+            'Built user interfaces for workflow management solutions and introduced a windows metro style design to new projects',
+            'Designed and developed reports for various workflow management systems',
+            'Develop and maintain customized workflow management software for various small to medium businesses',
+            'Performed data migration exercises when integrating or migrating from legacy systems',
+            'Worked with clients to troubleshoot and diagnose issues'
+          ],
+        }
       ],
       badges: ['C#', 'ASP.NET MVC', 'ASP.NET Web Forms', 'SQL Server', 'WPF', 'Windows forms', 'SQL Server Reporting Services', 'HTML5', 'JavaScript', 'CSS', 'jQuery', 'angularJS', 'Visual Basic']
     },
-    {
-      title: 'Student Intern',
+    {      
       company: 'Hansen Technologies',
       location: 'Auckland',
-      start: 'March 2013',
-      end: 'October 2013',
-      blurb: 'Worked as an intern doing a final year project to come up with a Business Intelligence solution using OLAP for Utility, Telecom and Pay TV clients.',
-      items: [
-        'Performed feasibility studies and investigations on various solutions such as Oracle OLAP, Pentaho Mondrian, Palo',
-        'Researched various OLAP implementations: MOLAP, ROLAP and HOLAP',
-        'Performed ETL based on sample data sets from Hansen\'s CISs (Customer Information System)',
-        'Developed a proof of concept solution consists of pivot tables, reports and dashboards'
-      ],
+      roles: [
+        {
+          title: 'Student Intern',
+          start: 'March 2013',
+          end: 'October 2013',
+          blurb: 'Worked as an intern doing a final year project to come up with a Business Intelligence solution using OLAP for Utility, Telecom and Pay TV clients.',
+          items: [
+            'Performed feasibility studies and investigations on various solutions such as Oracle OLAP, Pentaho Mondrian, Palo',
+            'Researched various OLAP implementations: MOLAP, ROLAP and HOLAP',
+            'Performed ETL based on sample data sets from Hansen\'s CISs (Customer Information System)',
+            'Developed a proof of concept solution consists of pivot tables, reports and dashboards'
+          ]
+        }
+      ],      
       badges: ['OLAP', 'ETL', 'Data Warehousing', 'Data Visualization', 'Java', 'MySQL']
     }
   ],
