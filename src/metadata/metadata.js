5e3d767079c59c6e9bab6f9f7345887542cc2174
fancycoconut <zodiacdagreat@outlook.com>
2025-04-04 23:37:36 +1300

    Update CV

--- src/metadata/metadata.js
+++ src/metadata/metadata.js
@@ -1,6 +1,6 @@
 module.exports = {
   name: 'Kawai Wong',
-  title: 'Full Stack Senior Developer',
+  title: 'Senior Software Engineer',
   facts: {
     'Residence': `<a href="https://goo.gl/maps/kToTNMXQgnmXDa5e8">
                   <i class="fa fa-home"></i> Auckland, New Zealand
@@ -19,53 +19,21 @@ module.exports = {
     {
       name: 'Technical Skills',
       examples: [
-        'Microsoft stack: C#, ASP.NET Core, Web API, MVC, EntityFramework',
-        'Modern Web stack: JavaScript, TypeScript, NodeJS, jQuery, React, Redux, Angular, HTML5, CSS3, WebPack, SASS/LESS, Bootstrap',
-        'Infrastructure: Azure, AWS, SQL Server, CosmosDB, DynamoDB, RabbitMQ, Docker, Kubernetes',
-        'Other: Python, Protobuf, CoAP',
-        'Experienced in web development & integration using industry standard technologies and frameworks',
-        'Experienced with all stages of the Software Development Life cycle and agile methodologies',
-        'Experienced and knowledge of best practices for software design and architecture'
+        'Backend: C#, .NET, ASP.NET, Web API, MVC',
+        'Frontend: JavaScript, TypeScript, NodeJS, React, HTML, CSS',
+        'Infrastructure: Azure, AWS, SQL Server, Postgres, CosmosDB, DynamoDB, RabbitMQ, Docker, Kubernetes, CI/CD Pipelines',
+        'Other: Python, Terraform, Pulumi, GraphQL, Protobuf, CoAP',
       ]
     },
     {
-      name: 'Leadership Skills',
+      name: 'Soft Skills',
       examples: [
-        'Lead developer for multiple microservices and other supporting IoT infrastructure at FPH & Pushpay',
-        'Proactive in taking up responsibilities within my expertise and knowledge area',
-        'Initiating proof of concepts and side projects to improve existing processes',
-        'Worked as a lead developer on large enterprise projects on multiple occasions at ASB Bank',
-        'Involved with project design workshops and meetings as a subject matter expert at ASB Bank'
+        'Leadership & feature leading',
+        'Can-do attitude',
+        'Analytical & problem solving',
+        'Adaptability and learning'
       ]
     },
-    {
-      name: 'Interpersonal and Communication Skills',
-      examples: [
-        'Client facing skills gained as a consultant',
-        'Excellent verbal and written communication skills',
-        'Confident in delivering business or team presentations',
-        'Experienced in writing detailed specifications and various forms of documentation',
-        'Fluent in English, Cantonese and Mandarin'
-      ]
-    },
-    {
-      name: 'Analytical and Problem-Solving Skills',
-      examples: [
-        'Able to break complicated business rules into simple or elegant solutions',
-        'Able to translate business requirements into system functionality',
-        'Estimate work and come up with best solution proposals',
-        'Able to troubleshoot complex issues and unfamiliar technologies'
-      ]
-    },
-    {
-      name: 'Learning Skills',
-      examples: [
-        'Adapting very quickly to various working environments',
-        'Build up relevant domain knowledge in a short time frame',
-        'Confident in learning and picking up new technology stacks and frameworks',
-        'Work on side projects in free time'
-      ]
-    }
   ],
   experience: [
     {
@@ -76,18 +44,19 @@ module.exports = {
           title: 'Senior Software Development Engineer',
           start: 'Dec 2021',
           end: 'Current',
-          blurb: 'Member of a high performing team that looks after APIs and external integrations for the Pushpay Giving platform',
+          blurb: 'Member of a high performing team responsible for Public APIs and integrations with the Pushpay Giving platform',
           items: [
-            'Feature lead Pushpay\'s next generation Web API platform',
-            'Involved with migration to microservices from a Monolith',
-            'Implement payment transaction syncing with external systems via vendor APIs',
-            'Taking ownership in supporting engineering experience through docker containers and other tooling/scripts',
-            'Involved with interviewing for recruitment',
-            'Involved with incident response, management and investigation'
+            'Feature lead Pushpay\'s next generation Public API platform which involves building new and migrating existing API endpoints as well as various supporting infrastructure',
+            'Co-develop a brand new authorization server for securing Public API endpoints',
+            'Built a new Authorization server that supports code and client credentials flow using the OAuth 2.0 protocol',
+            'Involved with the adoption of Open Telemetry for microservices and lambda functions',
+            'Worked on migrating integration functionality from a monolith to dedicated microservices to speedup development time and improve system resilience',
+            'Implement and enhance integrations to synchronize payments, transactions and community members with external systems via vendor APIs',
+            'Taking ownership in supporting engineering experience through docker containers and various other tooling/scripts',
           ]
         }
       ],
-      badges: ['C#', 'NET Core', 'ASP.NET Web API', 'Typescript', 'React', 'GraphQl', 'Postgres', 'DynamoDB', 'AWS', 'Terraform', 'Docker']
+      badges: ['C#', 'NET Core', 'ASP.NET Web API', 'Typescript', 'React', 'GraphQL', 'Postgres', 'DynamoDB', 'Lambda', 'ECS', 'S3', 'AWS', 'Terraform', 'Docker', 'Localstack', 'OAuth 2.0', 'Auth0', 'Duende']
     },
     {      
       company: 'Fisher & Paykel Healthcare',
@@ -97,14 +66,13 @@ module.exports = {
           title: 'Senior Software Engineer',
           start: 'May 2021',
           end: 'Nov 2021',
-          blurb: 'In the IoT team building supporting IoT infrastructure and APIs',
+          blurb: 'Member of the IoT team building supporting IoT infrastructure and microservices',
           items: [
             'Involved with and contributed towards the design of future system architecture and strategy',
-            'Involved with upgrading and dockerization of existing API service components',
+            'Involved with dockerization of existing API microservices',
             'Working with Operations to establish the containerization strategy for the use of Docker and Kubernetes within the team',
             'Setting up CI/CD pipelines and templates for migrating to Azure DevOps',
             'Mentor junior members on best practices of software design and architecture',
-            'Involved with interviewing for recruitment such as Developers and Solutions Architect roles',
             'Collaborating with firmware teams to solve problems in the IoT domain for medical devices'
           ]
         },
@@ -113,27 +81,19 @@ module.exports = {
           start: 'January 2018',
           end: 'April 2021',
           items: [
-            'Develop and contribute towards system Platform libraries',
-            'Rapidly prototyped new concepts and ideas',
-            'Involved with the delivery of customer integration projects that allows our clients to access our patient management APIs',
+            'Build and maintain Platform libraries and tooling that improve engineering experience',
             'Developed a parser for the CoAP protocol for both UDP and TCP variants for NET',
-            'Co-developed a gateway for IoT devices that integrates with the in-house communications protocol',
-            'Co-developed various infrastructure Web APIs',
-            //'Co-developed various infrastructure Web APIs for enabling a next generation over-the-air solution',
-            'Co-developed a dynamic serializer/deserializer library for Protobuf',        
-            'Involved with project delivery and coordination for the corporate website that required dealing with various stakeholders from Marketing Operations as well as a remote developer',
-            'Developed a custom module for the Kentico CMS used for bundling and packaging content for mobile apps',
-            'Setup CI/CD pipelines for different projects on TFS on-prem',
-            'Set up automated testing infrastructure such as SpecFlow and various other tooling such as project templates and test harnesses',
-            'Developed new features for a world class Obstructive Sleep Apnea patient management and monitoring cloud based solution',
+            'Help implement the in-house communications protocol to interface with IoT devices over cellular network',
+            'Co-developed and designed various microservices to enable a next generation over-the-air solution',
+            'Co-developed a dynamic serializer/deserializer library for Protobuf',            
             'Involved with the development and distribution of educational mobile apps for training purposes using React Native',
-            'Rewrote the existing user registration process for the SleepStyle iOS app to migrate from Objective-C to Swift and Storyboard',
+            'Rewrote the user registration process for the SleepStyle iOS app from Objective-C to Swift',
             'Involved with knowledge sharing of domain and system architecture to team members',
             'Experienced with design controls for medical devices'
           ],
         }
       ],      
-      badges: ['C#', 'NET Core', 'ASP.NET Web API', 'ASP.NET Web Forms', 'SQL Server', 'CosmosDB', 'Dapper', 'AutoMapper', 'Windsor Castle', 'EntityFramework', 'Identity Server', 'SpecFlow', 'Infrastructure', 'Docker', 'Azure Event Hub', 'RabbitMQ', 'Redis', 'Azure DevOps CI/CD', 'Swift', 'Objective-C', 'React Native', 'Protobuf', 'CoAP', 'Design Controls', 'ISO27001']
+      badges: ['C#', 'NET Core', 'ASP.NET Web API', 'ASP.NET Web Forms', 'SQL Server', 'CosmosDB', 'Dapper', 'AutoMapper', 'Windsor Castle', 'EntityFramework', 'Identity Server', 'SpecFlow', 'Docker', 'Kubernetes', 'Azure Event Hub', 'RabbitMQ', 'Redis', 'Azure DevOps CI/CD', 'Swift', 'Objective-C', 'React Native', 'Protobuf', 'CoAP', 'Design Controls', 'ISO27001']
     },
     {      
       company: 'Datacom',
@@ -145,18 +105,15 @@ module.exports = {
           end: 'December 2017',
           blurb: 'Assigned as a Software Developer to work on ASB Bank\'s CRM system in the domain of home loan origination and customer onboarding',
           items: [
-            'Designed and developed REST microservices for various CRM domains',
-            'Led the development and design of an automated test strategy for the CRM team',
-            'Designed and developed new components that added new functionality to the CRM',
-            'Involved as a lead developer and subject matter expert in large projects on multiple occassions to provide guidance and technical support',
-            'Delivered various compliance projects that are required by the Reserve Bank\'s legislative changes on time',
+            'Designed and developed REST microservices for the CRM domain',
+            'Built proof of concept for testing automation using Specflow for the CRM team',
+            'Involved as a lead developer and subject matter expert in multiple large projects to provide guidance and technical support',
+            'Delivered various critical compliance projects that are required by the Reserve Bank\'s legislative changes on time',
             'Involved with a large-scale enterprise upgrade project for ASB\'s CRM system',
-            'Experienced working with enterprise HOST systems',
-            'Worked on business-as-usual enhancements and bugfixes for Onyx'
           ]
         }        
       ],      
-      badges: ['C#', 'NET Core', 'ASP.NET Web API', 'ASP.NET Web Forms', 'SQL Server', 'WCF', 'SpecFlow', 'Onyx CRM', 'Octopus Deploy', 'Integration'],
+      badges: ['C#', 'NET Core', 'ASP.NET Web API', 'ASP.NET Web Forms', 'SQL Server', 'WCF', 'SpecFlow', 'Octopus Deploy'],
     },
     {
       className: 'screen',
@@ -171,7 +128,6 @@ module.exports = {
           items: [
             'Researched and developed an antenatal growth chart generator to integrate with the existing maternity management software',
             'Built user interfaces for workflow management solutions and introduced a windows metro style design to new projects',
-            'Designed and developed reports for various workflow management systems',
             'Develop and maintain customized workflow management software for various small to medium businesses',
             'Performed data migration exercises when integrating or migrating from legacy systems',
             'Worked with clients to troubleshoot and diagnose issues'
@@ -201,5 +157,5 @@ module.exports = {
       badges: ['OLAP', 'ETL', 'Data Warehousing', 'Data Visualization', 'Java', 'MySQL']
     }
   ],
-  interests: ['Technology', 'Music', 'Books', 'Sports & Fitness', 'Business', 'Finance']
+  interests: ['Technology', 'Music', 'Reading', 'Sports & Fitness', 'Business', 'Finance']
 };
